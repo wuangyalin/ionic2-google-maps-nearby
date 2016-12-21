1425ae90dfa0471c0f24754adf398e75a1acfba8
wuangyalin <wuangyalin@gmail.com>
2016-12-21 16:26:08 +1100

    First commit

---
+++ src/providers/google-maps.ts
@@ -0,0 +1,243 @@
+import { Injectable } from '@angular/core';
+import { Connectivity } from './connectivity';
+import { Geolocation } from 'ionic-native';
+
+declare var google;
+
+@Injectable()
+export class GoogleMaps {
+
+  mapElement: any;
+  pleaseConnect: any;
+  map: any;
+  mapInitialised: boolean = false;
+  mapLoaded: any;
+  mapLoadedObserver: any;
+  markers: any = [];
+  apiKey: string;
+
+  constructor(public connectivityService: Connectivity) {
+
+  }
+
+  init(mapElement: any, pleaseConnect: any): Promise<any> {
+
+    this.mapElement = mapElement;
+    this.pleaseConnect = pleaseConnect;
+
+    return this.loadGoogleMaps();
+
+  }
+
+  loadGoogleMaps(): Promise<any> {
+
+    return new Promise((resolve) => {
+
+      if(typeof google == "undefined" || typeof google.maps == "undefined"){
+
+        console.log("Google maps JavaScript needs to be loaded.");
+
+        this.disableMap();
+
+        if(this.connectivityService.isOnline()){
+
+          window['mapInit'] = () => {
+
+            this.initMap().then(() => {
+              resolve(true);
+            });
+
+            this.enableMap();
+          }
+
+          let script = document.createElement("script");
+          script.id = "googleMaps";
+
+          if(this.apiKey){
+            script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+          } else {
+            script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';       
+          }
+
+          document.body.appendChild(script);  
+
+        } 
+      }
+      else {
+
+        if(this.connectivityService.isOnline()){
+          this.initMap();
+          this.enableMap();
+        }
+        else {
+          this.disableMap();
+        }
+
+      }
+
+      this.addConnectivityListeners();
+
+    });
+
+  }
+
+  initMap(): Promise<any> {
+
+    this.mapInitialised = true;
+
+    return new Promise((resolve) => {
+
+      Geolocation.getCurrentPosition().then((position) => {
+
+        // UNCOMMENT FOR NORMAL USE
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        //let latLng = new google.maps.LatLng(40.713744, -74.009056);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement, mapOptions);
+        resolve(true);
+        google.maps.event.addListener(this.map, 'click', (event) => {
+          this.clearMarkers();
+          let geocoder = new google.maps.Geocoder;
+          let infowindow = new google.maps.InfoWindow;
+          let distanceToYou = this.getDistanceBetweenPoints(
+            event.latLng,
+            position,
+            'miles'
+          ).toFixed(2);
+          this.geocodeLatLng(event.latLng,geocoder,infowindow,distanceToYou);
+        });
+      });
+
+    });
+
+  }
+
+  disableMap(): void {
+
+    if(this.pleaseConnect){
+      this.pleaseConnect.style.display = "block";
+    }
+
+  }
+
+  enableMap(): void {
+
+    if(this.pleaseConnect){
+      this.pleaseConnect.style.display = "none";
+    }
+
+  }
+
+  addConnectivityListeners(): void {
+
+    document.addEventListener('online', () => {
+
+      console.log("online");
+
+      setTimeout(() => {
+
+        if(typeof google == "undefined" || typeof google.maps == "undefined"){
+          this.loadGoogleMaps();
+        } 
+        else {
+          if(!this.mapInitialised){
+            this.initMap();
+          }
+
+          this.enableMap();
+        }
+
+      }, 2000);
+
+    }, false);
+
+    document.addEventListener('offline', () => {
+
+      console.log("offline");
+
+      this.disableMap();
+
+    }, false);
+
+  }
+  addMarker(lat: number, lng: number): void {
+
+    let latLng = new google.maps.LatLng(lat, lng);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+    });
+
+    this.markers.push(marker);  
+
+  }
+  // Transfer latLng to specified Address 
+  geocodeLatLng(latLng: any,geocoder: any, infowindow:any,distanceToYou: any): void{
+    geocoder.geocode({'location': latLng}, (results, status) => {
+      if (status === 'OK') {
+        if (results[1]) {
+          let marker = new google.maps.Marker({
+            position: latLng,
+            animation: google.maps.Animation.DROP,
+            map: this.map
+          });
+          this.markers.push(marker);
+          infowindow.setContent(results[1].formatted_address+'<p style="color: red;">'+distanceToYou+" miles from your location</p>");
+          infowindow.open(this.map, marker);
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
+
+  // Removes the markers from the map, but keeps them in the array.
+  setMapOnAll(map) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+  clearMarkers() {
+    this.setMapOnAll(null);
+    this.markers = [];
+  }
+  //move this funciton from location.ts
+  getDistanceBetweenPoints(start, end, units){
+
+    let earthRadius = {
+      miles: 3958.8,
+      km: 6371
+    };
+
+    let R = earthRadius[units || 'miles'];
+    let lat1 = start.lat();
+    let lon1 = start.lng();
+    let lat2 = end.coords.latitude;
+    let lon2 = end.coords.longitude;
+
+    let dLat = this.toRad((lat2 - lat1));
+    let dLon = this.toRad((lon2 - lon1));
+    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    console.log('distanceToYou '+d);
+    return d;
+  }
+  toRad(x){
+    return x * Math.PI / 180;
+  }
+}
